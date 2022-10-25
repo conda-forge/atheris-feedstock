e1d5e14dd377560b551857f877e3aa347602f928
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-10-25 16:09:24 -0500

    fix bad jinja var

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,4 @@ export CLANG_BIN=$BUILD_PREFIX/bin/clang
 $CLANG_BIN --version
 # build and install the wheel to avoid issues on MacOS
 python setup.py bdist_wheel --dist-dir=dist
-python -m pip install --find-links=dist --no-deps --ignore-installed --no-cache-dir -vvv {{ name }}"
+python -m pip install --find-links=dist --no-deps --ignore-installed --no-cache-dir -vvv atheris
