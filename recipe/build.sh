bfaaeea56247da9a0b5ef974cb03ec74c8a776fd
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-10-25 16:04:59 -0500

    build and install wheel directly for MacOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,6 @@ set -eux
 export CLANG_BIN=$BUILD_PREFIX/bin/clang
 
 $CLANG_BIN --version
-
-python -m pip install . -vv
+# build and install the wheel to avoid issues on MacOS
+python setup.py bdist_wheel --dist-dir=dist
+python -m pip install --find-links=dist --no-deps --ignore-installed --no-cache-dir -vvv {{ name }}"
